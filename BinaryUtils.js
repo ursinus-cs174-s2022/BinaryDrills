f8a0e42eb2674c8ef33b121f5395b850ce51a7ac
Chris Tralie <chris.tralie@gmail.com>
2020-03-21 17:31:41 -0400

    Adding binary <==> hex example

--- BinaryUtils.js
+++ BinaryUtils.js
@@ -108,6 +108,11 @@ HEX_VAL_TO_NUM["D"] = 13;
 HEX_VAL_TO_NUM["E"] = 14;
 HEX_VAL_TO_NUM["F"] = 15;
 
+/**
+ * 
+ * @param {int} x A number in base 10
+ * @returns A hex string corresponding to x 
+ */
 function base10ToHex(x) {
     let rem = x;
     let b = "";
@@ -122,6 +127,11 @@ function base10ToHex(x) {
     return b;
 }
 
+/**
+ * 
+ * @param {string} h A hex string
+ * @returns An int corresponding to this hex string
+ */
 function hexToBase10(h) {
     let s = h.split("").reverse().join("");
     let place = 1;
@@ -133,7 +143,11 @@ function hexToBase10(h) {
     return x;
 }
 
-
+/**
+ * 
+ * @param {string} h A hex string
+ * @returns HTML code for a table showing how to convert this to base 10
+ */
 function makeHexTableStr(h) {
     let s = "";
     s += "<table border = 1 cellpadding=10><tr>";
@@ -150,10 +164,21 @@ function makeHexTableStr(h) {
     return s;
 }
 
+/**
+ * 
+ * @param {string} h A hex string
+ * @returns HTML code showing how to sum up places to convert this
+ *          to base 10
+ */
 function makeShowHexSumStr(h) {
+    let x = hexToBase10(h);
     let s = "<h2>";
-    place = Math.pow(16, h.length-1);
+    let place = Math.pow(16, h.length-1);
+    let hasLetters = false;
     for (let i = 0; i < h.length; i++) {
+        if (HEX_VAL_TO_NUM[h[i]] > 9) {
+            hasLetters = true;
+        }
         s += "<font color=red>" + h[i] + "</font>*<font color=green>" + place+"</font>";
         if (i < h.length - 1) {
             s += "+ ";
@@ -164,6 +189,79 @@ function makeShowHexSumStr(h) {
         place /= 16;
     }
     s + "</code><BR>"
-    s += "<b>" + hexToBase10(h) + "</b></h2>";
+    s += "<b>" + x + "</b></h2>";
+
+    // If there are letters in it, show an extra line to convert
+    // the letters to numbers
+    if (hasLetters) {
+        s += "In other words...<h2>"
+        place = Math.pow(16, h.length-1);
+        for (let i = 0; i < h.length; i++) {
+            s += "<font color=red>" + HEX_VAL_TO_NUM[h[i]] + "</font>*<font color=green>" + place+"</font>";
+            if (i < h.length - 1) {
+                s += "+ ";
+            }
+            else {
+                s += " = ";
+            }
+            place /= 16;
+        }
+        s + "</code><BR>"
+        s += "<b>" + x + "</b></h2><BR>";
+    }
+    return s;
+}
+
+
+
+
+
+
+/**
+ * 
+ * @param {string} b A binary string
+ * @returns The corresponding hext string
+ */
+function binaryToHex(b) {
+    let x = binaryToBase10(b);
+    return base10ToHex(x);
+}
+
+/**
+ * 
+ * @param {string} h A hex string
+ * @returns The corresponding binary string
+ */
+function hexToBinary(h) {
+    let x = hexToBase10(h);
+    return base10ToBinary(x);
+}
+
+/**
+ * 
+ * @param {string} h A hex string
+ * @returns A string with HTML code to create a table showing
+ *          how to convert each hexadecimal digit to bits
+ *          4 bits at a time 
+ */
+function makeHexBinaryTableStr(h) {
+    let s = "";
+    s += "<table border = 1 cellpadding=10><tr>";
+    for (let i = 0; i < h.length; i++) {
+        s += "<td><h3>" + h[i] + "</h3></td>";
+    }
+    s += "</tr><tr>";
+    for (let i = 0; i < h.length; i++) {
+        s += "<td><h3>";
+        let b = hexToBinary(h[i]);
+        for (let j = 0; j < 4-b.length; j++) {
+            s += "0";
+        }
+        for (let j = 0; j < b.length; j++) {
+            s += b[j];
+        }
+        s += "</h3></td>";
+    }
+    s += "</tr></table>";
     return s;
 }
